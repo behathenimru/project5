85564c7838a45984be4180924df4085a660eefb8
behathenimru <behaxsh@live.com>
2020-11-14 12:44:31 +0330

    student_list

--- student_list.py
+++ student_list.py
@@ -1,3 +1,4 @@
+#list_students=[{"username":"", "password":"", "money":"", "food":{}}]
 list_students=[
     {"username":"narges", "password":"12345", "money":10, "food":{}},
     {"username":"maryam", "password":"23456", "money":10, "food":{}},
