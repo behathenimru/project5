d36c07270c86fcdda90e8385e4382e2c8bbffb8b
maryamtufan <tufanpourm@yahoo.com>
2020-11-15 23:05:08 +0330

    student_csv_file.py changed

--- student_csv_file.py
+++ student_csv_file.py
@@ -2,7 +2,7 @@ import csv
 import random
 with open('studen_user_pass.csv', 'w', newline='') as file:
     writer = csv.writer(file)
-    writer.writerow(["user", "password"])
+    writer.writerow(["user", "password","hash(pass)"])
     ch=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r",\
         "s","t","u","v","w","x","y","z","@","#","$","&","*"]
     for i in range(9910010,9910041):
