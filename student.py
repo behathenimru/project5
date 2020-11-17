4165b0c44fa00bdf05a44af773d77c20e5cefc50
behathenimru <behaxsh@live.com>
2020-11-17 08:06:55 +0330

    student login

---
+++ student.py
@@ -0,0 +1,13 @@
+import csv
+import studen_user_pass.csv
+
+class Login:
+    def __init__(self, username , password):
+        self.username=username
+        self.password=password
+
+    def login():
+        with open('studen_user_pass.csv', 'r') as csv_file:
+            reader=csv.reader(csv_file)
+            for row in reader:
+                print(row)
